f38d391239392e83992d186a8ffa52d3afc85aa7
hafsanuu-gif <hafsanuu@mail.com>
2025-12-08 14:22:29 +0000

    I have learned data types today

---
+++ data_types.py
@@ -0,0 +1,19 @@
+# data_types.py
+# 1. Integer
+user_age = 55
+
+# 2. Float
+bank_balance = 1050.75
+
+# 3. String 
+favorite_color = "blue"
+
+# 4. Boolean (using a comparison)
+is_adult = user_age >= 30
+
+
+#print the values to see the output
+print("age:", user_age)
+print("Balance:", bank_balance)
+print("Color:", favorite_color)
+print("Is Adult:",is_adult)
